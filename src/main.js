6cd51d8a8f5fa853c46a13c830061b1334a92221
Pedro Henrique <pedrofuturama620@gmail.com>
2024-04-23 18:54:53 -0300

    Criando a plataforma e a funcao de pular

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import kaboom from "kaboom"
 
 kaboom()
 
+var largura = 48;
+
 loadSprite("bean", "sprites/bean.png");
+
+setGravity(1600)
 
 scene("game", () => {
     const jogador = add([
@@ -11,6 +15,25 @@ scene("game", () => {
         body(),
         area(),
     ]);
+
+    add([
+        rect(width(), largura),       
+        pos(0, height()),
+        area(),
+        body({isStatic: true}),
+        anchor("botleft"),
+        outline(4),
+        color(0, 8, 255),
+    ]);
+
+    function pular(){
+        if(jogador.isGrounded()){
+            jogador.jump(800);
+        }
+    }
+
+    onKeyPress("space", pular);
+    onClick(pular);
 
 })
 
